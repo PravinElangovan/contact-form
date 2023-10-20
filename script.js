3e04e6d2d9ae3e5d6c8c2cf4f2a3d6a2b09fbbd2
PravinElangovan <pravinkumarelangovan20@gmail.com>
2023-10-20 15:06:17 +0530

    first commit

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ tagButtons.forEach((button) => {
 
 function sendEmail(){
     Email.send({
+        secureToken : "e9f66688-4c55-43f9-844d-d0f297e98628",
         Host : "smtp.elasticemail.com",
         Username : "pravinelangovan20@gmail.com",
         Password : "0E53F12F7358905F91F2B57019D5573DCE65",
-        To : 'pravinelangovan20@gmail.com',
+        To : 'pravin.bussiness@gmail.com',
         From : "pravinelangovan20@gmail.com",
         Subject : "This is the subject",
         Body : "And this is the body"
